ec8735104eaaf0f33f2a5eede57d27c6a5fecd8b
lrpineda <luicks212@gmail.com>
2022-06-05 17:53:25 -0600

    initializing server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
