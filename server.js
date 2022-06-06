1ede239f5e71085109ae2fa87c52636bd093f15d
lrpineda <luicks212@gmail.com>
2022-06-05 22:11:20 -0600

    fixing issue with api

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
